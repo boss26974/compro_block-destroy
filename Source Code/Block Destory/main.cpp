b4f19ec4259d1168efd48cda4b61fed51cf15772
phalat1590 <56551048+phalat1590@users.noreply.github.com>
2020-05-08 02:42:58 +0700

    Source Code

---
+++ Source Code/Block Destory/main.cpp
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+    // Create the game object
+    Game* game = new Game();
+
+    // Initialize and run the game
+    if (game->Init()) {
+        game->Run();
+    }
+
+    // Clean up
+    delete game;
+    return 0;
+}
